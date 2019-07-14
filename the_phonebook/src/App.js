ca2c256384e19e9dc256193687dcfe2f1b52544e
ivoryblakk <george.ingram1@outlook.com>
2019-07-14 17:21:46 -0400

    first commit of phonebook

---
+++ the_phonebook/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  let copy = [...persons];
+
+  const addPersonToPhonebook = (event) => {
+    event.preventDefault()
+    copy.push({name: newName})
+    setPersons(copy)
+    setNewName("")
+    console.log('copy', copy)
+    console.log('person', persons)
+    console.log('button clicked', event.target)
+  }
+
+  const List =()=> {
+    const rows = copy.map( c => <Contact key={c.name} name={c.name} />)
+    return (
+      <div>
+        {rows}
+      </div>
+    )
+  }
+
+  const Contact = props => {
+    const {name} = props
+    return <div> {name}</div>;
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPersonToPhonebook}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>{List()}</div>
+    </div>
+  )
+}
+
+export default App
